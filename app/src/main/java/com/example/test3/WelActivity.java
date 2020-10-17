2188062c350c107168ec59c11d7585e271f73b31
sirius-ye <2420298154@qq.com>
2020-10-17 18:07:01 +0800

    信息和欢迎页面

--- app/src/main/java/com/example/test3/WelActivity.java
+++ app/src/main/java/com/example/test3/WelActivity.java
@@ -2,13 +2,19 @@ package com.example.test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelActivity extends AppCompatActivity {
 
+    private TextView wel_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wel);
+        wel_name=(TextView)findViewById(R.id.wel_name);
+        Intent intent=getIntent();
+        Bundle bundle=intent.getBundleExtra("login");
     }
 }
